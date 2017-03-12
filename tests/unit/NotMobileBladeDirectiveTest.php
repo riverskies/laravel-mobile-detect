c3fccad0f81b2f0a9cd20a768ee1e0a9fc67a3ae
Barnabas Kecskes <barnabas@riverskies.com>
2017-03-12 09:11:44 +0000

    Add NotMobile directive

---
+++ tests/unit/NotMobileBladeDirectiveTest.php
@@ -0,0 +1,56 @@
+<?php
+
+use Riverskies\Laravel\MobileDetect\NotMobileBladeDirective;
+
+class NotMobileBladeDirectiveTest extends TestCase
+{
+    /**
+     * Set up the world.
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->setUpTemplateEngine(new NotMobileBladeDirective);
+    }
+
+    /** @test */
+    public function it_will_not_render_if_mobile()
+    {
+        $this->expectReturn(true, false);
+
+        $html = $this->blade->view()->make('test')->render();
+
+        $this->assertEquals('', $this->clean($html));
+    }
+
+    /** @test */
+    public function it_will_render_if_tablet()
+    {
+        $this->expectReturn(false, true);
+
+        $html = $this->blade->view()->make('test')->render();
+
+        $this->assertEquals('<h1>Test</h1>', $this->clean($html));
+    }
+
+    /** @test */
+    public function it_will_render_if_desktop()
+    {
+        $this->expectReturn(false, false);
+
+        $html = $this->blade->view()->make('test')->render();
+
+        $this->assertEquals('<h1>Test</h1>', $this->clean($html));
+    }
+
+    /** @test */
+    public function it_will_display_else_if_exist_and_mobile()
+    {
+        $this->expectReturn(true, false);
+
+        $html = $this->blade->view()->make('test-else')->render();
+
+        $this->assertEquals('<h1>Else</h1>', $this->clean($html));
+    }
+}
